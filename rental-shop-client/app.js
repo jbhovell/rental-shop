7fd347ff8bfd295a0d00d05f45c452fbe9ee8caf
jianfangbhbbc <jianfang.bladen-hovell@bbc.co.uk>
2021-03-27 20:53:15 +0000

    changed view from pug to ejs

--- rental-shop-client/app.js
+++ rental-shop-client/app.js
@@ -5,14 +5,14 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var clientRouter = require('./routes/client');
+var usersRouter = require('./routes/users');
+var rentalsRouter = require('./routes/rentals');
 
 var app = express();
 
-
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
+app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -21,15 +21,16 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/client', clientRouter);
+app.use('/users', usersRouter);
+app.use('/rentals', rentalsRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
