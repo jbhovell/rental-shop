7d889389ee4a51e53fd219b668999f020ce29eb6
jianfangbhbbc <jianfang.bladen-hovell@bbc.co.uk>
2021-03-27 14:24:38 +0000

    cleaned up api and client

---
+++ rental-shop-client/routes/rentals.js
@@ -0,0 +1,24 @@
+var express = require('express');
+var router = express.Router();
+const axios = require('axios')
+/* GET users listing. */
+router.get('/', async function (req, res, next) {
+  //cors forbid?
+  //display in a table? pug front end
+  let data = (await axios.get('http://localhost:3001/api/cost')).data
+  if (req.query.user) {
+    let userData = {}
+    userData[req.query.user] = data[req.query.user]
+    data = userData
+  }
+  res.status(200).json(data);
+});
+
+router.post('/', () => {
+  //axios.post? all?
+})
+
+router.post('/customers', () => {
+
+})
+module.exports = router;
